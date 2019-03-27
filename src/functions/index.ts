90aba501b8534d1fc21201ca0487e002d37a9a4e
se1ch <faketofu712@gmail.com>
2019-03-27 23:28:35 +0900

    corsに対応

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,11 @@
 import { https } from "firebase-functions";
 import apolloServer from "./apollo";
 
-const handler = apolloServer.createHandler();
+const handler = apolloServer.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
 
 export const apollo = https.onRequest(handler);
